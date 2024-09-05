115c1ca10388a2a9021e36687c3d784ad79efbdd
SriMoorthy <srimoorthy27@gmail.com>
2024-09-06 00:04:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function asyncFun() {
 	let Time = parseInt(document.getElementById('delay').value)
 	let text = document.getElementById('text').value
 	await delay(Time)
-	document.getElementById('output').textcontent = text
+	document.getElementById('output').textContent = text;
 }
 
 document.getElementById('btn').addEventListener('click', asyncFun);
