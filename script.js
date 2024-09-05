9ddfd776940234e658c3d318f78ecaa3d5120b5a
SriMoorthy <srimoorthy27@gmail.com>
2024-09-05 23:58:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function asyncFun() {
+	let Time = parseInt(document.getElementByID('delay').value)
+	let text = document.getElementById('text').value
+	await delay(Time)
+	document.getElementById('output').textcontent = text
+}
+
+document.getElementById('btn').addEventListener('click', asyncFun);
